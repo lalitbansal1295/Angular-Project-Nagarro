bd6bed660ae5dd4fe1b1e319e853561f883797ac
Lalit Bansal <lalit.bansal@nagarro.com>
2020-04-21 20:20:19 +0530

    first commit

---
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -0,0 +1,22 @@
+import { Component,Input } from '@angular/core';
+import { Todo } from 'src/app/03_pipes/models/todo.interface';
+
+
+@Component({
+  selector: 'app-assignment2',
+  templateUrl: './assignment2.component.html',
+  styleUrls: ['./assignment2.component.css']
+})
+export class Assignment2Component {
+  @Input()
+  todoItem: Todo[];
+ 
+ @Input()
+ selectedTodo:Todo;
+ 
+ 
+ 
+ onselectTodo(todo : Todo) {
+   this.selectedTodo = todo;
+}
+}
